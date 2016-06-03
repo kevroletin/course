824b72799313ca21feed22997771664efc4dbc66
Vasiliy Kevroletin <kevroletin@gmail.com>
2016-06-03 23:16:35 +1000

    Solve compse
    Monad can't be implemented since it is most restrictive
    class from 3 listed.

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Course.Compose where
 
@@ -11,22 +12,21 @@ import Course.Monad
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
 newtype Compose f g a =
-  Compose (f (g a))
+  Compose (f (g a)) deriving (Eq, Show)
 
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+  (<$>) h (Compose x) = Compose $ (h <$>) <$> x
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  pure :: a -> Compose f g a
+  pure x = Compose $ pure (pure x)
+
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (<*>) (Compose h) (Compose x) = Compose $ lift2 (<*>) h x
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
